00083aba78895a7a05f6bf80a66a3fee13a5fceb
muskan <muskantuteja714@gmail.com>
2025-09-28 00:05:30 +0530

    Start Ticked app

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,313 @@
-import logo from './logo.png'
-
+import logo from "./logo.png";
 
 export const assets = {
   logo,
+
 }
+
+export const dummyTrailers = [
+  {
+    image: "https://assets-prd.ignimgs.com/2024/12/03/snow-white-2025-replacement-button-1733251158379.jpg",
+    videoUrl: "https://www.youtube.com/watch?v=iV46TJKL8cU",
+   
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/en/5/54/Avatar_The_Way_of_Water_poster.jpg",
+    videoUrl: "https://www.youtube.com/watch?v=9gghKcpVxd4",
+  },
+  {
+    image: "https://m.media-amazon.com/images/M/MV5BNDAzMDVhYjktMTk2ZS00Zjk3LWI4YWUtMDY1NjI5MDgxYzk5XkEyXkFqcGc@._V1_.jpg",
+    videoUrl: "https://www.youtube.com/watch?v=Dq6PmipyFuE",
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00378770-fshzjevtnf-portrait.jpg",
+    videoUrl: "https://www.youtube.com/watch?v=6bg5tM2jmUU&list=RD6bg5tM2jmUU&start_radio=1",
+  },
+ 
+];
+
+
+
+// src/assets/assets.js
+
+const dummyCastsData = [
+  {
+    "name": "Robert Downey Jr."," profile_path":
+      "https://akns-images.eonline.com/eol_images/Entire_Site/2012716/1024.ironman.jlc.081612.jpeg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top",
+  },
+  {
+    "name": "Azaad."," profile_path":
+      "https://cms.patrika.com/wp-content/uploads/2025/01/Upcoming-Movies-in-January-2025.jpg",
+  },
+  {
+    "name": "Rakkayie"," profile_path":
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA5lVkxLc2ZW_QNyTMcm1u01daICBIdzOYsy6xVaP47Igp8mSXEn1gDWlIczFyfkqVpF4&usqp=CAU",
+  },
+  {
+    "name": "Ghaati"," profile_path":
+      "https://m.media-amazon.com/images/M/MV5BZmZlYjY5NDctMDk5NS00MDY0LWE5MzUtMzViZWNhYjI4NjI1XkEyXkFqcGc@._V1_.jpg",
+  },
+  {
+    "name": "Obela"," profile_path":
+      "https://images.filmibeat.com/img/popcorn/list_items/upcoming-telugu-big-budget-movies-releasing-in-summer-2025-%E2%80%93-full-list-inside-20250331110945-7211.jpg",
+  },
+  {
+    "name": "Son of Sardaar"," profile_path":
+      "https://m.media-amazon.com/images/M/MV5BMjRjODY3MTQtZGQ3Ny00N2Y3LTk3MmUtZTFkOWU1ZTg1MjcxXkEyXkFqcGc@._V1_.jpg",
+  },
+  {
+    "name": "Avatar"," profile_path":
+      "https://www.tallengestore.com/cdn/shop/products/Avatar_9ea2c3a8-930b-4c06-866f-7e417ddde0d7.jpg?v=1634645786",
+  },
+  {
+    "name": "Jumanji"," profile_path":
+      "https://qqcdnpictest.mxplay.com/pic/bce7ae02445dad432bdab581e180ceef/en/2x3/312x468/d5f863cd13cc307123989701f8b72fdf_1280x1920.webp",
+  },
+  {
+    "name": "stree2"," profile_path":
+      "https://s1.dmcdn.net/v/WsQc01d5_t7P7yly0/x1080",
+  },
+  {
+    "name": " Cocktail"," profile_path":
+      "https://images004-a.media.erosnow.com/movie/1/1004611/img145210/6893375/1004611_6893375.jpg",
+  },
+  {
+    "name": "Tanvi The Great"," profile_path":
+      "https://filmfare.wwmindia.com/content/2025/jul/bollywood-movies-to-watch-in-july-2025-tanvi-the-great.jpg",
+  },
+  {
+    "name": "metro"," profile_path":
+      "https://img.etimg.com/thumb/msid-122092321,width-480,height-360,imgsize-47266,resizemode-75/metro-in-dino.jpg",
+  },
+  
+];
+
+export const dummyShowData = [
+  {
+     "_id": "1",
+     "id": 1,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":6.516,
+    "runtime":114
+
+
+
+  },
+  {
+    "_id": "2",
+     "id": 2,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  },
+  {
+     "_id": "3",
+     "id": 3,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":8.516,
+    "runtime":120
+  },
+  {
+    "_id": "4",
+     "id": 4,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  },
+  {
+    "_id": "5",
+     "id": 5,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  },
+  {
+    "_id": "6",
+     "id": 6,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  },
+  {
+    "_id": "7",
+     "id": 7,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  },
+  {
+    "_id": "8",
+     "id": 8,
+     "title":"In the list Lands",
+     "overview":"hello movie",
+     "poster_path": "https://m.media-amazon.com/images/S/pv-target-images/c60f68ca378775d0512b366fde20309bf62397641820b4389659a7fa20ae85bf.jpg",
+    "backdrop_path":"https://m.media-amazon.com/images/M/MV5BYTg4NGNmNzctZGE5NC00ODgyLWEwNTUtMThiOTdkZDUyZWM0XkEyXkFqcGc@._V1_.jpg",
+    "genres":[
+      {"id":28 , "name":"action"},
+      {"id":14 , "name":"Fantasy"},
+      {"id":12 , "name":"Adventure"}
+    ],
+    "casts" : dummyCastsData,
+    "release_date": "2025-5-12",
+    "original_language": "en",
+    "tagline" : "Be There and be square",
+    "vote_average":7.516,
+    "runtime":117
+  }
+ 
+]
+
+export const dummyDateTimeData={
+  "2025-5-12":[
+    {
+    "showId": 101,
+      "time": "2025-5-12T01:00:00.000Z",
+      "seatsAvailable": 50
+    },
+    {
+    "showId": 102,
+      "time": "2025-5-12T03:00:00.000Z",
+      "seatsAvailable": 50
+    },
+    {
+    "showId": 103,
+      "time": "2025-5-12T07:00:00.000Z",
+      "seatsAvailable": 50
+    },
+
+  ]
+}
+export const dummyBookingData = [
+  {
+    "_Id": "B001",        
+   " user": {"name": "muskan",},
+   
+    "show":{
+      showId: 102,
+      movie : dummyShowData[0],
+      showDateTimetime: "2025-5-12T03:00:00.000Z",
+      showPrice:59
+
+    },
+    "amount":98,
+    "bookedSeats":["D1","D2"],
+    "isPaid": false,
+    
+  },
+  {
+    "_Id": "B002",        
+  " user": {"name": "muskan",},
+  
+    "show":{
+      showId: 101,
+      movie : dummyShowData[0],
+      showDateTimetime: "2025-5-12T01:00:00.000Z",
+      showPrice:99
+
+    },
+    "amount":198,
+    "bookedSeats":["A1"],
+    "isPaid": true,
+    
+  },
+  {
+    "_Id": "B003",        
+ "user": {"name": "muskan",},
+   
+    "show":{
+      showId: 103,
+      movie : dummyShowData[0],
+      showDateTimetime: "2025-5-12T03:00:00.000Z",
+      showPrice:49
+
+    },
+    "amount":88,
+    "bookedSeats":["A1","A2","A3"],
+    "isPaid": false,
+    
+  },
+]
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { CalendarIcon, ClockIcon } from 'lucide-react'
+import { ArrowBigRight, CalendarIcon, ClockIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 
 const HeroSection = () => {
+
+const navigate = useNavigate()
   return (
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
-      <img src="" alt="" className='max-h-11 lg:h-11 mt-20'/>
-      <h1 className='text-5xl md:-[70px] md:leading-18 font-semibold max-w-110'>Book Now</h1>
+    
+      <h1 className='text-5xl md:-[70px] md:leading-18 font-semibold max-w-110'>Grab Your Seats <br />Catch Every Show</h1>
       <div className='flex items-center gap-4 text-gray-300'>
         <span>Action | Adventure | Sci-Fi</span>
         <div className='flex items-center gap-1'>
@@ -18,6 +21,10 @@ const HeroSection = () => {
         </div>
 
       </div>
+      <p className='max-w-md text-gray-300'>Experience the thrill of unstoppable heroes, epic battles, and jaw-dropping adventures on the big screen. Book your tickets now and be part of the action like never before</p>
+      <button onClick = {()=> navigate('/movies')}className='flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Explore Movies
+        <ArrowBigRight className='w-5 h-5'/>
+      </button>
       
     </div>
   )
