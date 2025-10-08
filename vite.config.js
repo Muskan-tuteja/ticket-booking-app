e2e2d38dd120fd0dbad84bad85f80bd918c78a7e
muskan <muskantuteja714@gmail.com>
2025-10-08 23:18:37 +0530

    8/10/25

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ticket-booking-app/', // <-- add this line
   plugins: [react(), tailwindcss()],
- 
 })
+
