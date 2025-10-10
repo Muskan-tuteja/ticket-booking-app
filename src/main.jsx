542cb2126598a7f5db3c7161534f0f727ca5316b
muskan <muskantuteja714@gmail.com>
2025-10-10 23:59:31 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/ticket-booking-app" >
+    <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
+  // <BrowserRouter basename="/ticket-booking-app" >
+  //   <App />
+  // </BrowserRouter>
 );
