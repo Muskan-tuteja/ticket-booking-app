ebc8180d042270043bd2fdd5629f8dd4aa12da24
muskan <muskantuteja714@gmail.com>
2025-10-22 14:41:53 +0530

    22/10/25

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,10 +13,10 @@ const HeroSection = () => {
       <div className="flex items-center gap-4 text-gray-300">
         <span>Action | Adventure | Sci-Fi</span>
         <div className="flex items-center gap-1">
-          <CalendarIcon className="w-4.5 h-4.5" /> 2018
+          <CalendarIcon className="w-4.5 h-4.5" /> 2025
         </div>
         <div className="flex items-center gap-1">
-          <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
+          <ClockIcon className="w-4.5 h-4.5" /> 0h 0m
         </div>
       </div>
       <p className="max-w-md text-gray-300">
